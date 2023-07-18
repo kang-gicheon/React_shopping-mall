e814be5b2ac0aae32205334f5805a63c508970c1
kanggicheon <snoopy1516@naver.com>
2023-07-18 23:34:31 +0900

    메인페이지 상품 이미지 적용테스트
    img / src -> 이미지 출력 여부 테스트

--- shop/src/App.js
+++ shop/src/App.js
@@ -15,7 +15,28 @@ function App() {
       </Navbar>
 
       {/* src 메인페이지 이미지 적용 테스트 / 이미지 태그를 사용한다면 <img src = {변수명}> */}
-      <div className='main-bg'></div> 
+      <div className='main-bg'></div>
+
+      {/* 쇼핑몰 grid 영역 */}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src = "https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>1번 상품</h4>
+            <p>1번 상품입니다</p>
+          </div>
+          <div className="col-md-4">
+            <img src = "https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>2번 상품</h4>
+            <p>2번 상품입니다</p>
+          </div>
+          <div className="col-md-4">
+            <img src = "https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>3번 상품</h4>
+            <p>3번 상품입니다</p>
+          </div>
+        </div>
+      </div> 
 
     </div>
   );
