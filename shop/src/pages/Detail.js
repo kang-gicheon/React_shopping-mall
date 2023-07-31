89dd6d26a4258cba3b8f34f1f45d038612fc8fa9
kanggicheon <snoopy1516@naver.com>
2023-07-31 23:36:24 +0900

    useEffect 활용 기능 추가 연습
    setTimeout을 적용한 사라지는 창 구현,
    isNaN을 활용한 숫자만 받게 하는 input태그 구현

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
+  let [count, setCount] = useState(0);
+  let [talert, setTalert] = useState(true);
+  let [put, setPut] = useState(true);
+  let [num, setNum] = useState("");
   let { id } = useParams();
+
+  let fproduct = props.shoes.find((x) => x.id == id);
+
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setTalert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isNaN(num) == true) {
+      alert("에헤이 그러지마라");
+    }
+  }, [num]);
 
   return (
     <div className="container">
+      {talert == true ? (
+        <div className="alert alert-warning">2초 이내 구매시 할인</div>
+      ) : null}
+      {count}
+      <button onClick={() => setCount(count + 1)}>버튼</button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -13,6 +40,11 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}원</p>
